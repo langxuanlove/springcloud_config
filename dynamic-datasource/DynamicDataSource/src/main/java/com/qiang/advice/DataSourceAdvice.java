0c73a9d04c5357fc38265812bff943c591600acd
Kui <langxuanlove@users.noreply.github.com>
2017-11-21 16:26:51 +0800

    大神写的收藏下

---
+++ dynamic-datasource/DynamicDataSource/src/main/java/com/qiang/advice/DataSourceAdvice.java
@@ -0,0 +1,47 @@
+package com.qiang.advice;
+
+import java.lang.reflect.Method;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+import com.qiang.anno.DataSourceEnum;
+import com.qiang.anno.DateSource;
+import com.qiang.datasource.DataSourceSwitcher;
+
+@Aspect
+public class DataSourceAdvice {
+
+	/**
+	 * ����Ϊfinal String���͵�,ע����Ҫʹ�õı���ֻ���Ǿ�̬�������͵�
+	 */
+	public static final String EDP = "execution(* com.qiang.dao.*.*(..))";
+
+	@After(EDP)
+	public void afterReturning() {
+		DataSourceSwitcher.setSlave();
+	}
+
+	@Before(EDP)
+	public void before(JoinPoint joinPoint) {
+		System.out.println("=================");
+		String methodNm = joinPoint.getSignature().getName();
+		Method[] methods = joinPoint.getTarget().getClass().getMethods();
+		for (Method method : methods) {
+			if (method.getName().equals(methodNm)) {
+				DateSource d = method.getAnnotation(DateSource.class);
+				if (d != null) {
+					DataSourceEnum value = d.value();
+					if (value.equals(DataSourceEnum.MASTER)) {
+						DataSourceSwitcher.setMaster();
+					} else {
+						DataSourceSwitcher.setSlave();
+					}
+				}
+				break;
+			}
+		}
+	}
+}
